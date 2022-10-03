c10394976e6e4733ebd8d54fc6a89b972e3e5c46
Togumogu <toygun.baysal@hotmail.com>
2022-10-03 23:39:33 +0300

    Cleaned the code a little bit still having trouble with the prompt but for sure i got the randomized computer results and a round.

--- javascript.js
+++ javascript.js
@@ -9,58 +9,75 @@ function getComputerChoice() {
     let computerSelection = ""
     let randomSelection = Math.floor(Math.random() * 3)
     if (randomSelection === 0) {
-        computerSelection = "Rock"
+        computerSelection = "rock"
     } else if (randomSelection === 1) {
-        computerSelection = "Scissors"
+        computerSelection = "scissors"
     } else if (randomSelection === 2) {
-        computerSelection = "Paper"
+        computerSelection = "paper"
     }
     return computerSelection
 }
 
 //Player Choice
 
-function pl
+function getPlayerChoice() {
+    let playerSelection = prompt("Rock, paper or scissors?")
+    playerSelection = playerSelection.toLowerCase()
+    if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
+        return playerSelection
+    } else {
+        return "Please enter either rock, paper or scissors!"
+    }
+}
 
 //A Single Round of Rock-Paper-Scissors
 
 function singleRound(playerSelection,computerSelection) {
+    
+
+   
     if (playerSelection === computerSelection) {
-        console.log("Tie!")
+        return "Tie"
     } else if (
         (playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
-        (playerSelection === "scissors" && computerSelection === "paper")) {
-            playerScore +=
-            console.log(`You Win! ${playerSelection} beats ${computerSelection}!`)
+        (playerSelection === "scissors" && computerSelection === "paper")
+        ) {
+            return `You Win! ${playerSelection} beats ${computerSelection}!`
+            playerScore++
         } else if (
         (playerSelection === "scissors" && computerSelection === "rock") ||
         (playerSelection === "rock" && computerSelection === "paper") ||
-        (playerSelection === "paper" && computerSelection === "scissors")) {
-            computerScore +=
-            console.log(`You Lose! ${computerSelection} beats ${playerSelection}!`)
+        (playerSelection === "paper" && computerSelection === "scissors")
+        ) {
+            return `You Lose! ${computerSelection} beats ${playerSelection}!`
+            computerScore++
         } else {
-            console.log("Enter either rock, paper or scissors!")
+            return "Something went wrong!"
         }
+    
+    checkScore()
+
+}
+    
+//Score Check
+
+function checkScore() {
+    if ((playerScore < 5) && (computerScore < 5)) {
+        singleRound()
+    } else {
+        endgame()
     }
-
-//Game of 5 Rounds
-
-function rockPaperScissors(
-    singleRound()
-    singleRound()
-    singleRound()
-    singleRound()
-    singleRound()
-)
-
+}
 
 //Endgame
 
 function endgame() {
     if (playerScore === 5) {
-        console.log("Game is over! You Win")
+        return "Game is over! You Win"
     } else if (computerScore === 5) {
-        console.log("Game is over! You Lose")
+        return "Game is over! You Lose"
     }
-}
+    playerScore = 0
+    computerScore = 0
+}   
