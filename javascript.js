a7330f7531414c0180bb19b4e16305de54a0b4c0
Togumogu <toygun.baysal@hotmail.com>
2022-10-04 01:53:53 +0300

    Finally figured out why the prompt did not work. Simple syntax error, forgot to close bracket, wasted half an hour. Oh well, also figured out I wasn't using the correct variables in the loop, I was using the same variables in the function essentially leaving it withput a value. Learned a lot today! But I think it is working, definetely cannot say I didn't cheat but I figure I created something that is something that I can call my own.

--- javascript.js
+++ javascript.js
@@ -6,61 +6,102 @@ let computerScore = 0
 //Computer Randomized Choice
 
 function getComputerChoice() {
-    let computerChoice = ""
+
     let randomSelection = Math.floor(Math.random() * 3)
+    let choice
+
     if (randomSelection === 0) {
-        computerChoice = "rock"
+        choice = "rock"
     } else if (randomSelection === 1) {
-        computerChoice = "scissors"
+        choice = "scissors"
     } else if (randomSelection === 2) {
-        computerChoice = "paper"
+        choice = "paper"
     }
-    return computerChoice
+
+    return choice;
+
 }
 
 //Player Choice
 
 function getPlayerChoice() {
-    let playerChoice = prompt("Rock, paper or scissors?", "")
-    playerChoice = playerChoice.toLowerCase()
-    if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
-        return playerChoice
+
+    let choice = prompt("Rock, paper or scissors?", "")
+    choice = choice.toLowerCase()
+    
+    if (choice === "rock" || choice === "paper" || choice === "scissors") {
+       return choice
     } else {
-        return "Please enter either rock, paper or scissors!"
+        alert("Invalid Entry!!! Rock, paper or scissors?", "")
+        getPlayerChoice()
     }
+
 }
 
-//A Single Round of Rock-Paper-Scissors
+///A Single Round of Rock-Paper-Scissors
 
-function singleRound(playerChoice,computerChoice) {
-   
+function singleRound(playerChoice, computerChoice) {
 
+    console.log(`You: ${playerChoice} --- Computer: ${computerChoice}`)
+    
     if (playerChoice === computerChoice) {
-        return "Tie"
-    } else if (
+        console.log("Tie")
+        } else if (
         (playerChoice === "rock" && computerChoice === "scissors") ||
         (playerChoice === "paper" && computerChoice === "rock") ||
         (playerChoice === "scissors" && computerChoice === "paper")
         ) {
-            return `You Win! ${playerChoice} beats ${computerChoice}!`
+            console.log(`You Win! ${playerChoice} beats ${computerChoice}!`)
             playerScore++
         } else if (
         (playerChoice === "scissors" && computerChoice === "rock") ||
         (playerChoice === "rock" && computerChoice === "paper") ||
         (playerChoice === "paper" && computerChoice === "scissors")
         ) {
-            return `You Lose! ${computerChoice} beats ${playerChoice}!`
+            console.log(`You Lose! ${computerChoice} beats ${playerChoice}!`)
             computerScore++
         } else {
-            return "Something went wrong!"
+            console.log("Something went wrong!")
         }
-
+        
+}
 //Game of Rock Paper Scissors
 
 function rockPaperScissors() {
-    for (let i = 1, i < 6, i++) {
+
+    for (let i = 1; i < 6; i++) {
         playerSelection = getPlayerChoice()
         computerSelection = getComputerChoice()
-        singleRound()
+        singleRound(playerSelection, computerSelection)
+        showResults(i)
     }
+
+    winner()
+
 }
+
+//Show Results
+
+function showResults(round) {
+    console.log(`Round ${round} of 5. Get Ready!`)
+    console.log(`You: ${playerScore} --- Computer: ${computerScore}`)
+}
+
+//Winner
+
+function winner() {
+
+    if (playerScore > computerScore) {
+        console.log("Congratulations! You Win!")
+    } else {
+        console.log("Sorry! You lose!")
+    }
+    
+}
+
+//Start Game
+
+rockPaperScissors()
+
+
+
